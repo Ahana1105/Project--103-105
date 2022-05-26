df8dd78a9148f9255e5980508a48c5149e340dbb
Ahana1105 <77888005+Ahana1105@users.noreply.github.com>
2022-05-26 20:15:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,4 +19,26 @@ document.getElementById("result").innerHTML="<img id='captured_img' src='"+data_
 };
 
 console.log(ml5.version);
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Rp648YDYe/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/d_IGOfUQN/model.json",modelLoaded);
+
+function modelLoaded() {
+    console.log("model is loaded");
+}
+
+
+function check() {
+
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("name_object").innerHTML = results[0].label;
+        document.getElementById("accuracy_object").innerHTML = results[0].confidence.toFixed(2);
+    }
+
+}
